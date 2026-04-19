65a84943314f5892336003e69158d8acc2b76c4b
Evelina22 <evelinasavalieva7@gmail.com>
2026-04-20 00:00:15 +0600

    fix host

--- backend/app.py
+++ backend/app.py
@@ -151,5 +151,4 @@ def reserve_item(item_id):
 
     return jsonify({"error": "Товар не найден"}), 404
 
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(host="0.0.0.0", port=5000, debug=False)
